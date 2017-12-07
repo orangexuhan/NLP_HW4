645c56929e45f71cee2b91854a463195b9b70e6e
unknown <orangexuhan@163.com>
2017-12-06 23:11:58 -0500

    3

--- src/properties.py
+++ src/properties.py
@@ -2,7 +2,7 @@ class NetProperties:
     def __init__(self, word_embed_dim, pos_embed_dim, label_embed_dim, hidden_dim_1, hidden_dim_2, minibatch_size):
         self.word_embed_dim = word_embed_dim
         self.pos_embed_dim = pos_embed_dim
+        self.label_embed_dim = label_embed_dim
         self.hidden_dim_1 = hidden_dim_1
         self.hidden_dim_2 = hidden_dim_2
         self.minibatch_size = minibatch_size
-        self.properties.label_embed_dim = label_embed_dim
