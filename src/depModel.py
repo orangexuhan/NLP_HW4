b9b182263d13535430bfa84e0894973b44292f55
unknown <orangexuhan@163.com>
2017-12-07 01:16:18 -0500

    13

--- src/depModel.py
+++ src/depModel.py
@@ -1,8 +1,12 @@
 import os,sys
+from optparse import OptionParser
 from decoder import *
+from network import *
+
+
 
 class DepModel:
-    def __init__(self):
+    def __init__(self, model_path, vocab_path):
         '''
             You can add more arguments for examples actions and model paths.
             You need to load your model here.
@@ -13,6 +17,12 @@ class DepModel:
         self.actions = ['SHIFT', 'LEFT-ARC:rroot', 'LEFT-ARC:cc', 'LEFT-ARC:number', 'LEFT-ARC:ccomp', 'LEFT-ARC:possessive', 'LEFT-ARC:prt', 'LEFT-ARC:num', 'LEFT-ARC:nsubjpass', 'LEFT-ARC:csubj', 'LEFT-ARC:conj', 'LEFT-ARC:dobj', 'LEFT-ARC:nn', 'LEFT-ARC:neg', 'LEFT-ARC:discourse', 'LEFT-ARC:mark', 'LEFT-ARC:auxpass', 'LEFT-ARC:infmod', 'LEFT-ARC:mwe', 'LEFT-ARC:advcl', 'LEFT-ARC:aux', 'LEFT-ARC:prep', 'LEFT-ARC:parataxis', 'LEFT-ARC:nsubj', 'LEFT-ARC:<null>', 'LEFT-ARC:rcmod', 'LEFT-ARC:advmod', 'LEFT-ARC:punct', 'LEFT-ARC:quantmod', 'LEFT-ARC:tmod', 'LEFT-ARC:acomp', 'LEFT-ARC:pcomp', 'LEFT-ARC:poss', 'LEFT-ARC:npadvmod', 'LEFT-ARC:xcomp', 'LEFT-ARC:cop', 'LEFT-ARC:partmod', 'LEFT-ARC:dep', 'LEFT-ARC:appos', 'LEFT-ARC:det', 'LEFT-ARC:amod', 'LEFT-ARC:pobj', 'LEFT-ARC:iobj', 'LEFT-ARC:expl', 'LEFT-ARC:predet', 'LEFT-ARC:preconj', 'LEFT-ARC:root', 'RIGHT-ARC:rroot', 'RIGHT-ARC:cc', 'RIGHT-ARC:number', 'RIGHT-ARC:ccomp', 'RIGHT-ARC:possessive', 'RIGHT-ARC:prt', 'RIGHT-ARC:num', 'RIGHT-ARC:nsubjpass', 'RIGHT-ARC:csubj', 'RIGHT-ARC:conj', 'RIGHT-ARC:dobj', 'RIGHT-ARC:nn', 'RIGHT-ARC:neg', 'RIGHT-ARC:discourse', 'RIGHT-ARC:mark', 'RIGHT-ARC:auxpass', 'RIGHT-ARC:infmod', 'RIGHT-ARC:mwe', 'RIGHT-ARC:advcl', 'RIGHT-ARC:aux', 'RIGHT-ARC:prep', 'RIGHT-ARC:parataxis', 'RIGHT-ARC:nsubj', 'RIGHT-ARC:<null>', 'RIGHT-ARC:rcmod', 'RIGHT-ARC:advmod', 'RIGHT-ARC:punct', 'RIGHT-ARC:quantmod', 'RIGHT-ARC:tmod', 'RIGHT-ARC:acomp', 'RIGHT-ARC:pcomp', 'RIGHT-ARC:poss', 'RIGHT-ARC:npadvmod', 'RIGHT-ARC:xcomp', 'RIGHT-ARC:cop', 'RIGHT-ARC:partmod', 'RIGHT-ARC:dep', 'RIGHT-ARC:appos', 'RIGHT-ARC:det', 'RIGHT-ARC:amod', 'RIGHT-ARC:pobj', 'RIGHT-ARC:iobj', 'RIGHT-ARC:expl', 'RIGHT-ARC:predet', 'RIGHT-ARC:preconj', 'RIGHT-ARC:root']
         # write your code here for additional parameters.
         # feel free to add more arguments to the initializer.
+        vocab, net_properties = pickle.load(open(vocab_path, 'r'))
+        # constructing default network
+        self.model = Network(vocab, net_properties)
+
+        # loading network trained model
+        self.model.load(model_path)
 
     def score(self, str_features):
         '''
@@ -22,10 +32,23 @@ class DepModel:
         :return: list of scores
         '''
         # change this part of the code.
-        return [0]*len(self.actions)
+        return self.model.build_graph(str_features)
 
 if __name__=='__main__':
-    m = DepModel()
+    parser = OptionParser()
+    parser.add_option("--model", dest="model_path", metavar="FILE", default=None)
+    parser.add_option("--vocab", dest="vocab_path", metavar="FILE", default=None)
+    parser.add_option("--we", type="int", dest="we", default=64)
+    parser.add_option("--pe", type="int", dest="pe", default=32)
+    parser.add_option("--le", type="int", dest="le", default=32)
+    parser.add_option("--hidden1", type="int", dest="hidden1", default=200)
+    parser.add_option("--hidden2", type="int", dest="hidden2", default=200)
+    parser.add_option("--minibatch", type="int", dest="minibatch", default=1000)
+
+    args = ["--model", "data/model.data", "--vocab", "data/vocabs.data"]
+    (options, args) = parser.parse_args(args)
+
+    m = DepModel(options.model_path, options.vocab_path)
     input_p = os.path.abspath(sys.argv[1])
     output_p = os.path.abspath(sys.argv[2])
     Decoder(m.score, m.actions).parse(input_p, output_p)
